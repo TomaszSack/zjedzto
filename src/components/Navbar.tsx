3ceb7bb629a29ac7bcba38653f304d56cedc89a7
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-12 15:32:29 +0100

    fixed tailwind, added boxshadow on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,16 @@ import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <div className="fixed h-20 w-full flex justify-center bg-white">
+    <div className="fixed h-20 w-full flex justify-center bg-white shadow-nav">
       <div className="h-full w-8/12 flex items-center justify-between">
         <img src={logo} className="h-4/6" />
         <div className="text-4xl">
           <span className="font-bold">Zjedz</span>To
         </div>
-        <FontAwesomeIcon className="h-2/6" icon={faCartShopping} />
+        <FontAwesomeIcon
+          className="h-2/6 text-primary-orange"
+          icon={faCartShopping}
+        />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-orange": "#FE9305",
+        "secondary-orange": "rgba(254, 147, 5, 0.5)",
+        "primary-pink": "#F562BE",
+        "secondary-pink": "rgba(245, 99, 187, 0.7)",
+      },
+      boxShadow: {
+        "nav": "0 0px 25px 0px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
-};
+}
