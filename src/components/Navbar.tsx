4d7941d2c7ba3f819d6b221e354aa95ffbefae73
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-12 13:36:26 +0100

    Created core navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import logo from "../logo-wbg.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
   return (
-    <div className="fixed h-16 w-full flex justify-center bg-white">
-      <div className="h-full w-9/12">Nav</div>
+    <div className="fixed h-20 w-full flex justify-center bg-white">
+      <div className="h-full w-8/12 flex items-center justify-between">
+        <img src={logo} className="h-4/6" />
+        <div className="text-4xl">
+          <span className="font-bold">Zjedz</span>To
+        </div>
+        <FontAwesomeIcon className="h-2/6" icon={faCartShopping} />
+      </div>
     </div>
   );
 };
