f2e3a4d161990616f7b5e29fba64aafb6dc4e347
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-12 16:19:54 +0100

    added routing to logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <div className="fixed h-20 w-full flex justify-center bg-white shadow-nav">
       <div className="h-full w-8/12 flex items-center justify-between">
-        <img src={logo} className="h-4/6" />
+        <a href="/" className="h-4/6">
+          <img src={logo} className="h-full" />
+        </a>
         <div className="text-4xl">
           <span className="font-bold">Zjedz</span>To
         </div>
