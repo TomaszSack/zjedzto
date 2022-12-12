eaaabb3be32dbb4629ac3f8c966be0296a5c0cd9
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-12 11:31:28 +0100

    creating core body of the project. Added tailwind

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <div className="fixed h-16 w-full flex justify-center bg-white">
+      <div className="h-full w-9/12">Nav</div>
+    </div>
+  );
+};
+export default Navbar;
