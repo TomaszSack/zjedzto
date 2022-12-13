d217762d21728eebfb189fc01382e669c13483fb
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-13 08:41:00 +0100

    some structural changes in files, used tailwind in Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,5 @@
-import "./Home.css";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return <div className="flex items-center justify-center text-center h-screen bg-gray-500">Home</div>;
 };
 export default Home;
