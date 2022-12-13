e0ce059ce84a18c2763e1d1a062b160f4d32eb4e
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-13 11:16:23 +0100

    created footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,20 @@
 import ContentContainer from "../layout/ContentContainer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare, faInstagram, faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <div className="bg-primary-gray">
-      <ContentContainer>Footer</ContentContainer>
+      <ContentContainer className="text-white text-4xl py-3 flex justify-between">
+        <div className="text-white space-x-4">
+          <FontAwesomeIcon icon={faFacebookSquare} />
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faSquareTwitter} />
+        </div>
+        <div>
+          <span className="font-bold">Zjedz</span>To
+        </div>
+      </ContentContainer>
     </div>
   );
 };
