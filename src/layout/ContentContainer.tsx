16f5c3446f637d0eac9795c1132bad75e7164b15
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-14 14:18:21 +0100

    changed type of children, added alt on logo

--- src/layout/ContentContainer.tsx
+++ src/layout/ContentContainer.tsx
@@ -1,4 +1,8 @@
-const ContentContainer = ({ className = "", children }: any) => (
+type Props = {
+  className: string;
+  children: React.ReactNode;
+};
+const ContentContainer: React.FC<Props> = ({ className, children }) => (
   <div className="flex justify-center h-full">
     <div className={`w-8/12 ${className}`}>{children}</div>
   </div>
