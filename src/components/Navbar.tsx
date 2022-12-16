d982c6cd3c20d86cf78fc8f734a871261a69f9dd
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-16 11:25:47 +0100

    fixed bouncing arrow

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import ContentContainer from "../layout/ContentContainer";
 
 const Navbar = () => {
   return (
-    <div className="fixed h-20 w-full bg-white shadow-nav">
+    <div className="fixed h-20 w-full bg-white shadow-nav z-10">
       <ContentContainer className="flex items-center justify-between">
         <a href="/" className="h-4/6">
           <img src={logo} className="h-full" />
