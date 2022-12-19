c9470c035e751b0b65ce1e0b08f6c7d7f4f8f8ce
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-19 16:04:00 +0100

    created basic size wrapper of order page

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -4,9 +4,15 @@ import PageWrapper from "layout/PageWrapper";
 const OrderPage = () => {
   return (
     <PageWrapper>
-      <ContentContainer>
-        <div className="py-24">order</div>
-      </ContentContainer>
+      <div className="h-screen bg-primary-white">
+        <ContentContainer className="flex flex-wrap items-center">
+          <div className="flex flex-wrap h-5/6 w-full">
+            <div className="h-5/6 w-1/2 bg-red-500"></div>
+            <div className="h-5/6 w-1/2 bg-red-300"></div>
+            <div className="h-1/6 w-full bg-red-400"></div>
+          </div>
+        </ContentContainer>
+      </div>
     </PageWrapper>
   );
 };
