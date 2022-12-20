f3175b2933f5ae28ae998c3f4b0bf496c79a7b62
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-20 14:21:15 +0100

    changed Button component, created order buttons

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,3 +1,4 @@
+import Button from "components/Button";
 import ContentContainer from "layout/ContentContainer";
 import PageWrapper from "layout/PageWrapper";
 
@@ -9,7 +10,14 @@ const OrderPage = () => {
           <div className="flex flex-wrap h-5/6 w-full">
             <div className="h-5/6 w-1/2 bg-red-500"></div>
             <div className="h-5/6 w-1/2 bg-red-300"></div>
-            <div className="h-1/6 w-full bg-red-400"></div>
+            <div className="flex flex-col justify-around items-center h-1/6 w-full">
+              <div>
+                <Button href="/summary" pink className="text-3xl py-2">Zamawiam</Button>
+              </div>
+              <div>
+                <Button cancel>Anuluj</Button>
+              </div>
+            </div>
           </div>
         </ContentContainer>
       </div>
