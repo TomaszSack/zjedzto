d676b92e2d937e67d799530c1d921327f5b8d6f4
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2022-12-20 14:47:03 +0100

    added key prop to .map productItems

--- src/components/Menu/ProductsList.tsx
+++ src/components/Menu/ProductsList.tsx
@@ -3,36 +3,42 @@ import img from "../../assets/hero-img.jpeg";
 
 const dishes = [
   {
+    id: 1,
     name: "Burger wołowy z bekonem",
     price: "32,99 zł",
     img,
     alt: "soczysty burger wołowy z frytkami",
   },
   {
+    id: 2,
     name: "Burger z kurczakiem",
     price: "30,99",
     img,
     alt: "soczysty burger wołowy z frytkami",
   },
   {
+    id: 3,
     name: "Burger wołowy na ostro",
     price: "35,99",
     img,
     alt: "soczysty burger wołowy z frytkami",
   },
   {
+    id: 4,
     name: "Burger wege",
     price: "27,99",
     img,
     alt: "soczysty burger wołowy z frytkami",
   },
   {
+    id: 5,
     name: "Frytki",
     price: "6,99",
     img,
     alt: "soczysty burger wołowy z frytkami",
   },
   {
+    id: 6,
     name: "Krążki cebulowe",
     price: "8,99",
     img,
@@ -47,6 +53,7 @@ const ProductsList = () => {
         {dishes.map((dish) => {
           return (
             <ProductItem
+              key={dish.id}
               name={dish.name}
               price={dish.price}
               img={dish.img}
