f4aa548e7a1b0a2d1c3cc19d270df1e9e58e921d
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2023-01-02 15:59:18 +0100

    added dynamic data

--- src/components/Order/OrderItem.tsx
+++ src/components/Order/OrderItem.tsx
@@ -10,9 +10,9 @@ const OrderItem: React.FC<Props> = ({img, name, price, quantity}) => {
         <div className="w-5/6 h-3/18 m-auto bg-tertiary-orange p-1 mb-4 rounded-lg">
             <div className="flex items-center justify-between w-full h-full bg-white rounded-lg overflow-hidden">
               <img src={img} className="h-full" />
-              <div>Burger wołowy z bekonem</div>
-              <div>39,99 zł</div>
-              <div>2 szt</div>
+              <div className="w-1/3 text-center">{name}</div>
+              <div className="text-xl">{price} zł</div>
+              <div>{quantity} szt</div>
               <div className="pr-4 text-red-400 font-bold text-2xl pb-2">x</div>
             </div>
           </div>
