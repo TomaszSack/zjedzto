9d6c516971e02d8f8302bd43ac4e3fe057f796ba
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2023-01-12 10:23:48 +0100

    created test with mocking data of ProductList

--- src/components/Menu/ProductsList.tsx
+++ src/components/Menu/ProductsList.tsx
@@ -4,29 +4,35 @@ import ProductItem from "./ProductItem";
 const url = "http://localhost:3000/menu";
 
 interface Products {
-  id:number;
+  id: number;
   name: string;
   price: string;
   img: string;
   alt: string;
-};
+}
 
 const ProductsList = () => {
   const [data, setData] = useState<Products[]>([]);
 
   useEffect(() => {
-    const dataFetch = async () => {
-      const data = await (await fetch(url)).json();
-      setData(data);
-    };
-    dataFetch();
+    try {
+      const dataFetch = async () => {
+        const data = await (
+          await fetch(url)
+        ).json();
+        setData(data);
+      };
+      dataFetch();
+    } catch {
+      (error: string) => console.error(error);
+    }
   }, []);
 
   return (
     <div className="flex justify-center">
       <div className="grid grid-cols-3 gap-12">
         {data.map((dish) => {
-          const {id, name, price, img, alt} = dish
+          const { id, name, price, img, alt } = dish;
           return (
             <ProductItem
               key={id}
--- src/components/__tests__/Menu.test.tsx
+++ src/components/__tests__/Menu.test.tsx
@@ -1,11 +1,61 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import MenuSection from "components/Menu/MenuSection";
 import ProductsList from "components/Menu/ProductsList";
 
+const data = [
+  {
+    id: 1,
+    name: "Burger wołowy z bekonem",
+    price: 32.99,
+    img: "assets/hero-img.jpeg",
+    alt: "soczysty burger wołowy z frytkami",
+  },
+  {
+    id: 2,
+    name: "Burger z kurczakiem",
+    price: 30.99,
+    img: "assets/hero-img.jpeg",
+    alt: "soczysty burger wołowy z frytkami",
+  },
+  {
+    id: 3,
+    name: "Burger wołowy na ostro",
+    price: 35.99,
+    img: "assets/hero-img.jpeg",
+    alt: "soczysty burger wołowy z frytkami",
+  },
+];
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+  jest.spyOn(global, "fetch").mockImplementation(
+    jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(data),
+      })
+    ) as jest.Mock
+  );
+});
+// const mockedFetch = jest.fn(() =>
+//   Promise.resolve({
+//     json: () => Promise.resolve(menu),
+//   })
+// ) as jest.Mock;
+
+// global.fetch = mockedFetch;
+
+// console.log(mockedFetch);
+
+test("should render fetched data", async () => {
+  await render(<ProductsList />);
+  // await screen.findByText("Burger wołowy z bekonem");
+  // await expect(fetch).toHaveBeenCalledTimes(1);
+  expect(
+    await screen.findAllByAltText("soczysty burger wołowy z frytkami")
+  ).toHaveLength(3);
+});
+
 test("should navigate to order page", () => {
   const { getByText } = render(<MenuSection />);
-  expect(getByText("Zamawiam").closest("a")).toHaveAttribute(
-    "href",
-    "/order"
-  );;
+  expect(getByText("Zamawiam").closest("a")).toHaveAttribute("href", "/order");
 });
