552f0733e8ebae243e85bb35fbbc4dba1375ee5b
TomaszSack <105498556+TomaszSack@users.noreply.github.com>
2023-02-13 14:02:39 +0100

    Feature/nav cart mobile (#19)
    * created navcart on mobile and ipad
    
    * minor change in lg view

--- src/components/Order/OrderItem.tsx
+++ src/components/Order/OrderItem.tsx
@@ -21,20 +21,39 @@ const OrderItem: React.FC<{ dish: DishProps; small?: boolean }> = ({
   const { id, name, price, quantity, img, alt } = dish;
 
   return (
-    <div 
+    <div
       className={
-        "bg-tertiary-orange rounded-lg " + 
-        (small 
-          ? "w-11/12 h-5/18 pb-1 m-auto mb-2" 
+        "bg-tertiary-orange rounded-lg " +
+        (small
+          ? "w-11/12 h-6/18 xl:h-5/18 pb-1 m-auto mb-2"
           : "lg:w-5/6 h-16 lg:h-3/18 p-1 m-3 sm:mx-8 lg:m-auto lg:mb-4")
-      }>
+      }
+    >
       <div className="flex items-center justify-between w-full h-full bg-white rounded-lg overflow-hidden">
         {!small && <img src={img} alt={alt} className="h-full" />}
-        <div className="w-1/4 lg:w-1/3 text-sm lg:text-lg text-center">
-          {name}
-        </div>
-        <div className="text-lg lg:text-xl">{price} zł</div>
-        <QuantityPicker min={0} max={9} value={quantity} dish={dish} small={small} />
+        {small ? (
+          <div className="flex flex-col justify-center lg:justify-around items-center lg:flex-row w-1/2 xl:w-7/12 3xl:w-8/12">
+            <div className="text-sm lg:text-base text-center lg:w-1/2">
+              {name}
+            </div>
+            <div className="text-base lg:text-base text-center lg:w-1/2">{price} zł</div>
+          </div>
+        ) : (
+          <>
+            <div className="w-1/4 lg:w-1/3 text-sm lg:text-lg text-center">
+              {name}
+            </div>
+            <div className="text-lg lg:text-xl">{price} zł</div>
+          </>
+        )}
+
+        <QuantityPicker
+          min={0}
+          max={9}
+          value={quantity}
+          dish={dish}
+          small={small}
+        />
         <FontAwesomeIcon
           onClick={() => removeFromCart(id)}
           icon={faXmark}
